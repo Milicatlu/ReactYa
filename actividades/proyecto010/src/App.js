ba498461fa4942ce8e0114f3cbaf5aee5ec0dda8
Milicatlu <luchianavillarroel8@gmail.com>
2023-04-16 19:03:04 -0300

    a

---
+++ actividades/proyecto010/src/App.js
@@ -0,0 +1,55 @@
+import { useState, useEffect } from 'react';
+
+function App() {
+
+  const [articulos, setArticulos] = useState([])
+  const [recuperado, setRecuperado] = useState(false)
+
+
+  function mostrarTabla() {
+    return (
+      <div>
+        <table border="1">
+          <thead>
+            <tr>
+              <th>Código</th>
+              <th>Descripción</th>
+              <th>Precio</th>
+            </tr>
+          </thead>
+          <tbody>
+            {articulos.map(art => {
+              return (
+                <tr key={art.codigo}>
+                  <td>{art.codigo}</td>
+                  <td>{art.descripcion}</td>
+                  <td>{art.precio}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+
+  useEffect(() => {
+    fetch('https://scratchya.com.ar/react/datos.php')
+      .then((response) => {
+        return response.json()
+      })
+      .then((articulos) => {
+        setTimeout(() => {
+          setArticulos(articulos)
+          setRecuperado(true)
+        }, 2000);
+      })
+  }, [])
+
+  if (recuperado)
+    return mostrarTabla()
+  else
+    return (<div>recuperando datos...</div>)
+}
+
+export default App
